e25b0f0e1c6b0837880398f8c082a2959f3d2f6e
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-05-07 13:33:13 -0500

    fixes

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -4,7 +4,8 @@ module.exports = {
     getThoughts(req, res){
         Thought.find()
         .then((thoughts) => res.json(thoughts))
-      .catch((err) => res.status(500).json(err));
+      .catch((err) => {
+        res.status(500).json(err)});
     },
     getThoughtId(req,res){
         Thought.findOne({_id: req.params.thoughtId})
